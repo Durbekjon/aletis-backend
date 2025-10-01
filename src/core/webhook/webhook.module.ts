7dda9742456d0ab13a8b2698c092b4e2a127bb53
Durbek Saydaliyev <121376739+Durbekjon@users.noreply.github.com>
2025-10-01 11:33:32 +0500

    Refactor webhook handling by replacing WebhookModule with WebhookHelperModule
    - Removed the existing WebhookModule and its service.
    - Introduced WebhookHelperModule and WebhookHelperService to manage webhook interactions.
    - Updated core module imports to utilize the new WebhookHelperModule.

--- src/core/webhook/webhook.module.ts
+++
@@ -1,8 +0,0 @@
-import { Logger, Module } from '@nestjs/common';
-import { WebhookService } from './webhook.service';
-
-@Module({
-  providers: [WebhookService, Logger],
-  exports:[WebhookService]
-})
-export class WebhookModule {}
